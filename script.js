6918a632511fc1cde597f341d0184b485cecd20f
emjayruka <105775319+emjayruka@users.noreply.github.com>
2023-12-10 18:10:04 +0100

    linked_

---
+++ script.js
@@ -0,0 +1,4 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=political,sexist&type=single";
+
